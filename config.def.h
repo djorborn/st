ac85e0f66342fb332699e97985a4bd8d6e2f7bb2
Daniel Osborn <djorborn@gmail.com>
2020-05-31 07:10:21 -0400

    dracula theme

--- config.def.h
+++ config.def.h
@@ -88,35 +88,75 @@ char *termname = "st-256color";
  *
  *	stty tabs
  */
-unsigned int tabspaces = 8;
+unsigned int tabspaces = 2;
 
+/* Terminal colors (16 first used in escape sequence) */
+//static const char *colorname[] = {
+//	/* 8 normal colors */
+//	"#3b4252", /* black   */
+//	"#bf616a", /* red     */
+//	"#a3be8c", /* green   */
+//	"#ebcb8b", /* yellow  */
+//	"#81a1c1", /* blue    */
+//	"#b48ead", /* magenta */
+//	"#88c0d0", /* cyan    */
+//	"#e5e9f0", /* white   */
+//
+//	/* 8 bright colors */
+//	"#4c566a", /* black   */
+//	"#bf616a", /* red     */
+//	"#a3be8c", /* green   */
+//	"#ebcb8b", /* yellow  */
+//	"#81a1c1", /* blue    */
+//	"#b48ead", /* magenta */
+//	"#8fbcbb", /* cyan    */
+//	"#eceff4", /* white   */
+//
+//	[255] = 0,
+//
+//	/* more colors can be added after 255 to use with DefaultXX */
+//	"#2e3440", /* background */
+//	"#d8dee9", /* foreground */
+//};
+//
+//
+///*
+// * Default colors (colorname index)
+// * foreground, background, cursor, reverse cursor
+// */
+//unsigned int defaultfg = 257;
+//unsigned int defaultbg = 256;
+//static unsigned int defaultcs = 257;
+//static unsigned int defaultrcs = 256;
+
+/* ---------------------------------------------------------------------------------------- */
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
 	/* 8 normal colors */
-	"#3b4252", /* black   */
-	"#bf616a", /* red     */
-	"#a3be8c", /* green   */
-	"#ebcb8b", /* yellow  */
-	"#81a1c1", /* blue    */
-	"#b48ead", /* magenta */
-	"#88c0d0", /* cyan    */
-	"#e5e9f0", /* white   */
+	"#3f3f3f", /* black   */
+	"#ff5555", /* red     */
+	"#50da7b", /* green   */
+	"#f1fa8c", /* yellow  */
+	"#bde9fd", /* blue    */
+	"#ff79c6", /* magenta */
+	"#8be9fd", /* cyan    */
+	"#dfdfdf", /* white   */
 
 	/* 8 bright colors */
-	"#4c566a", /* black   */
-	"#bf616a", /* red     */
-	"#a3be8c", /* green   */
-	"#ebcb8b", /* yellow  */
-	"#81a1c1", /* blue    */
-	"#b48ead", /* magenta */
-	"#8fbcbb", /* cyan    */
-	"#eceff4", /* white   */
+	"#4d4d4d", /* black   */
+	"#ff6e67", /* red     */
+	"#5af78e", /* green   */
+	"#f4f99d", /* yellow  */
+	"#caa9fa", /* blue    */
+	"#ff92d0", /* magenta */
+	"#9aedfe", /* cyan    */
+	"#e6e6e6", /* white   */
 
 	[255] = 0,
 
 	/* more colors can be added after 255 to use with DefaultXX */
-	"#2e3440", /* background */
-	"#d8dee9", /* foreground */
+	"#44475a", /* background */
+	"#fafafa", /* foreground */
 };
 
 
@@ -129,6 +169,9 @@ unsigned int defaultbg = 256;
 static unsigned int defaultcs = 257;
 static unsigned int defaultrcs = 256;
 
+/* ---------------------------------------------------------------------------------------- */
+
+
 /*
  * Default shape of cursor
  * 2: Block ("█")
@@ -136,6 +179,7 @@ static unsigned int defaultrcs = 256;
  * 6: Bar ("|")
  * 7: Snowman ("☃")
  */
+
 static unsigned int cursorshape = 2;
 
 /*
